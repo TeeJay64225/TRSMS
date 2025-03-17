80a1b22cb2d1aea461bda1bd83caf08b6fae5e13
TeeJay64225 <kwesilit@gmail.com>
2025-03-17 03:40:25 +0000

    back

--- JS/adminDashboardFunction.js
+++ JS/adminDashboardFunction.js
@@ -1,6 +1,6 @@
 async function fetchTotalUsers() {
     try {
-        const response = await fetch('https://trsms-db.onrender.com/api/user/count'); // Ensure backend route exists
+        const response = await fetch('https://trsms-db.onrender.com/api/user'); // Ensure backend route exists
         if (!response.ok) throw new Error('Network response was not ok');
         
         const data = await response.json();
